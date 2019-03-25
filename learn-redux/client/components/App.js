2b75bc9019586f2530b272822295bc78f38c551f
skobraf <skobraf@gmail.com>
2019-03-25 15:54:14 +0000

    connect react with redux

---
+++ learn-redux/client/components/App.js
@@ -0,0 +1,13 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+const mapStateToProps = (state) => ({
+    posts: state.posts,
+    comments: state.comments
+})
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(actionCreators, dispatch);
+
+const App = connect(mapStateToProps, mapDispatchToProps);
