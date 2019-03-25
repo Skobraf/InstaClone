8ed4a14515b41192aec868b817e7ae2f8c8599b9
skobraf <skobraf@gmail.com>
2019-03-25 14:56:37 +0000

    added actions

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+// increment
+export const increment = (index) => (
+    {
+    type:'INCREMENT_LIKES',
+    index
+    }
+)
+
+// add comment
+export const addComent = (postId, author, comment) => (
+    {
+        type:'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+)
+
+// remove comment 
+export const removeComment = (postId, i) => (
+    {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+) 
