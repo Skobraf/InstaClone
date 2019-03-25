cfdfe08d0aeeb5939affe8fb27f804953e869cbf
skobraf <skobraf@gmail.com>
2019-03-25 20:44:05 +0000

    single photo component

---
+++ learn-redux/client/components/Photo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Photo extends React.Component {
+    render() {
+        return (
+            <figure className="grid-figure" >
+                I'm a photo
+            </figure>
+        )
+    }
+}
+
+export default Photo;
