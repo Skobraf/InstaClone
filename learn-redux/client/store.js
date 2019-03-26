8c67e30bcc60d14f212c8adbf8d777597479b0bf
skobraf <skobraf@gmail.com>
2019-03-26 14:49:15 +0000

    fix comment state

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -3,8 +3,9 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 import rootReducer from './reducers/index';
-import comments from './reducers/index';
+import comments from './data/comments';
 import posts from './data/posts';
+
 
 const defaultState = {
     posts,
